dfce889345d08afd692aa1be550e86978598d30d
Andrés P <Tovkal@gmail.com>
2015-09-27 18:39:48 +0200

    Center bus line bubbles

--- SaldoEMT/Presentation/ViewControllers/BusLineCollectionViewController.swift
+++ SaldoEMT/Presentation/ViewControllers/BusLineCollectionViewController.swift
@@ -8,75 +8,19 @@
 
 import UIKit
 
-private let reuseIdentifier = "BusLine"
-
 class BusLineCollectionViewController: UICollectionViewController {
     
-    private var busLines = [BusLine(number: "1", color: UIColor.redColor()), BusLine(number: "2", color: UIColor.yellowColor())]
+    private let reuseIdentifier = "BusLine"
+    
+    private var busLines = [BusLine(number: "1", color: UIColor.redColor()),
+                            BusLine(number: "2", color: UIColor.yellowColor()),
+                            BusLine(number: "3", color: UIColor.blueColor())]
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Register cell classes
         self.collectionView!.registerClass(BusLineView.self, forCellWithReuseIdentifier: reuseIdentifier)
-        self.collectionView!.contentSize = CGSizeMake(100, 100)
-
-        // Do any additional setup after loading the view.
-        
-        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using [segue destinationViewController].
-        // Pass the selected object to the new view controller.
-    }
-    */
-
-
-
-    // MARK: UICollectionViewDelegate
-
-    /*
-    // Uncomment this method to specify if the specified item should be highlighted during tracking
-    override func collectionView(collectionView: UICollectionView, shouldHighlightItemAtIndexPath indexPath: NSIndexPath) -> Bool {
-        return true
-    }
-    */
-
-    /*
-    // Uncomment this method to specify if the specified item should be selected
-    override func collectionView(collectionView: UICollectionView, shouldSelectItemAtIndexPath indexPath: NSIndexPath) -> Bool {
-        return true
-    }
-    */
-
-    /*
-    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
-    override func collectionView(collectionView: UICollectionView, shouldShowMenuForItemAtIndexPath indexPath: NSIndexPath) -> Bool {
-        return false
-    }
-
-    override func collectionView(collectionView: UICollectionView, canPerformAction action: Selector, forItemAtIndexPath indexPath: NSIndexPath, withSender sender: AnyObject?) -> Bool {
-        return false
-    }
-
-    override func collectionView(collectionView: UICollectionView, performAction action: Selector, forItemAtIndexPath indexPath: NSIndexPath, withSender sender: AnyObject?) {
-    
-    }
-    */
-
 }
 
 // MARK: UICollectionViewDataSource
@@ -99,5 +43,15 @@ extension BusLineCollectionViewController {
         
         return cell
     }
+}
 
+// MARK: UICollectionViewDelegateFlowLayout
+
+extension BusLineCollectionViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAtIndex section: Int) -> UIEdgeInsets {
+        
+        // Center bus line bubbles horizontally
+        let edgeInsets = (self.view.frame.width - (CGFloat(busLines.count) * 50) - (CGFloat(busLines.count) * 10)) / 2
+        return UIEdgeInsetsMake(0, edgeInsets, 0, 0);
+    }
 }

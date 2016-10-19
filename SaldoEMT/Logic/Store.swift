0c326e0bbc15935f8afb8992895c57c7e7dacaed
tovkal <Tovkal@gmail.com>
2016-10-19 22:20:20 +0200

    WIP Download fares from S3

--- SaldoEMT/Logic/Store.swift
+++ SaldoEMT/Logic/Store.swift
@@ -140,10 +140,14 @@ class Store {
     // MARK: - Init
     
     fileprivate init() {
-        if let json = getFileData() {
+        
+        // TODO Rethink this bit
+        /*if let json = fetchFares() {
             parseBusLines(json)
             parseFares(json)
-        }
+        }*/
+        
+        fetchFares()
         
         initBalance()
     }
@@ -160,6 +164,67 @@ class Store {
     }
     
     // MARK: - JSON Processing
+    
+    fileprivate func processData(json: JSON) {
+        print(json)
+        parseBusLines(json)
+        parseFares(json)
+    }
+    
+    fileprivate func fetchFares() {
+        let endpoint: String = "https://s3.eu-central-1.amazonaws.com/saldo-emt/fares_es.json"
+        guard let url = URL(string: endpoint) else {
+            print("Error: cannot create URL")
+            return
+        }
+        let urlRequest = URLRequest(url: url)
+        
+        // set up the session
+        let config = URLSessionConfiguration.default
+        let session = URLSession(configuration: config)
+        
+        // make the request
+        let task = session.dataTask(with: urlRequest) {
+            (data, response, error) in
+            // check for any errors
+            guard error == nil else {
+                print("error fetching fares")
+                print(error)
+                return
+            }
+            // make sure we got data
+            guard let responseData = data else {
+                print("Error: did not receive data")
+                return
+            }
+            
+            self.processData(json: JSON(data: responseData))
+            // parse the result as JSON, since that's what the API provides
+            /*do {
+                //return JSON(data: responseData)
+                guard let todo = try JSONSerialization.jsonObject(with: responseData, options: []) as? [String: AnyObject] else {
+                    print("error trying to convert data to JSON")
+                    return
+                }
+                // now we have the todo, let's just print it to prove we can access it
+                print("The todo is: " + todo.description)
+                
+                // the todo object is a dictionary
+                // so we just access the title using the "title" key
+                // so check for a title and print it if we have one
+                guard let todoTitle = todo["title"] as? String else {
+                    print("Could not get todo title from JSON")
+                    return
+                }
+                print("The title is: " + todoTitle)
+            } catch  {
+                print("error trying to convert data to JSON")
+                return
+            }*/
+        }
+        
+        task.resume()
+    }
     
     fileprivate func getFileData() -> JSON? {
         if let path = Bundle.main.path(forResource: "fares_es", ofType: "json") {
